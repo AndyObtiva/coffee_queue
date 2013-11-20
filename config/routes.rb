aafeb95bb9046772cb439dd81e7fbb60203f2fd4
Andy Maleh <andy.am@gmail.com>
2013-11-20 16:47:30 -0500

    Opened up paths for assets by constraining batman.js to root only

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ CoffeeQueue::Application.routes.draw do
 
   resources :orders
 
-  get "(*redirect_path)", to: "batman#index", constraints: lambda { |request| request.format == "text/html" }
+  #get "(*redirect_path)", to: "batman#index", constraints: lambda { |request| request.format == "text/html" }
+  root to: "batman#index", constraints: lambda { |request| request.format == "text/html" }
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
