014a11d18f55faad242d20493c682974dd1f9948
Andy Maleh <andy.am@gmail.com>
2013-11-20 17:27:10 -0500

    Making batman.js work by switching on asset compilation in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ CoffeeQueue::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true #enabled temporarily to make Batman.js work TODO figure out how to make it work without it
 
   # Generate digests for assets URLs.
   config.assets.digest = true
